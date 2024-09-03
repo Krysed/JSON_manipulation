b1f59b2ceb7739ce24d6a64cc5a9d3bbd607df7b
Krystian Fryca <kp15731@gmail.com>
2024-09-03 22:17:43 +0200

    Added base program flow

--- source/app.py
+++ source/app.py
@@ -1,40 +1,71 @@
 """App module"""
 import os
+import sys
+import factories
+import json_creator
 
-def define_globals():
-    global json_file
-
-class app:    
+class App:
+    """App class that controls flow of the program."""
     def __init__(self):
+        # Initialize the path to the JSON file
         self.json_file = os.path.join("sample.json")
 
     def start_app(self):
         """Main loop for the program."""
         while True:
             self.show_menu()
-            action = int(input("Choice: "))
-            self.choose_action(action=action)
+            try:
+                action = int(input("Choice: "))
+                self.choose_action(action)
+            except ValueError:
+                print("Invalid input. Please enter a number.")
 
     def show_menu(self):
         """Display the menu options to the user."""
-        print("Choose action to execute:")
-        print("1. ")
-        print("2. ")
-        print("3. ")
-        print("4. Exit the app.")
+        print("Choose an action to execute:")
+        print("1. Add Fake Users")
+        print("2. List All Users")
+        print("3. Update/Delete User")
+        print("4. Exit")
 
-    def choose_action(self,action):
+    def choose_action(self, action):
+        """Execute the selected action based on user input."""
         if action == 1:
-            pass
+            self.add_fake_users()
         elif action == 2:
-            pass
+            self.list_users()
         elif action == 3:
-            pass
+            self.update_or_delete_user()
         elif action == 4:
             self.exit_action()
         else:
-            print("No such option available.")
+            print("No such option available. Please choose a valid option.")
+
+    def add_fake_users(self):
+        """Add two fake users using the factories module."""
+        for _ in range(2):
+            user = factories.create_fake_user()
+            json_creator.insert_user(user, self.json_file)
+        print("Two fake users have been added.")
+
+    def list_users(self):
+        """List all users from the JSON file."""
+        print("Listing all users:")
+        json_creator.list_users(self.json_file)
+
+    def update_or_delete_user(self):
+        """Update or delete the first user in the list for demonstration purposes."""
+        users = json_creator.read_json_file(self.json_file)
+        if not users:
+            print("No users available to update or delete.")
+            return
+
+        print(f"Users: {users}")
+
+        # Update user
+        # Delete user
 
     def exit_action(self):
+        """Exiting an application"""
         print("Exiting..")
-        exit()
+        sys.exit()
--- source/main.py
+++ source/main.py
@@ -1,12 +1,9 @@
 """Main script running the app"""
 import app
-import factories
-import json_creator
-
 
 if __name__ == "__main__":
 
-    instance = app.app()
+    instance = app.App()
     instance.start_app()
 
     exit()
